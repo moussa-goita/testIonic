e026a1aeef36f5cdd05353a2d7739ed17e411622
reyptz <reypotozy@gmail.com>
2024-08-08 23:19:12 +0000

    Dark mode, Sidemenu, Tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { addIcons } from 'ionicons';
+import { homeOutline, homeSharp, cubeOutline, cubeSharp, enterOutline, enterSharp, exitOutline, exitSharp, pricetagsOutline, pricetagsSharp, keyOutline, keySharp, businessOutline, businessSharp, peopleOutline, peopleSharp, personOutline, personSharp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [CommonModule, RouterModule, IonicModule,NavigationMenuComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    IonicModule,
+    NavigationMenuComponent,
+    RouterLink,
+    RouterLinkActive,
+    FormsModule
+  ],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  isDarkMode = false;
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.isDarkMode = localStorage.getItem('darkMode') === 'true' || prefersDark;
+    console.log('Dark Mode on Init:', this.isDarkMode);
+    document.body.classList.toggle('dark', this.isDarkMode);
+    document.body.classList.add('dark');
+  }
+
+
+  public appPages = [
+    { title: 'Dashboard', url: '/dashboard', icon: 'home' },
+    { title: 'Produits', url: '/produit-list', icon: 'cube' },
+    { title: 'Bons d\'Entrée', url: '/bon-entree-list', icon: 'enter' },
+    { title: 'Bons de Sortie', url: '/bon-sortie-list', icon: 'exit' },
+    { title: 'Fournisseurs', url: '/fournisseur-list', icon: 'people' },
+  ];
+
+  constructor() {
+    addIcons({
+      homeOutline, homeSharp,
+      cubeOutline, cubeSharp,
+      enterOutline, enterSharp,
+      exitOutline, exitSharp,
+      pricetagsOutline, pricetagsSharp,
+      keyOutline, keySharp,
+      businessOutline, businessSharp,
+      peopleOutline, peopleSharp,
+      personOutline, personSharp
+    });
+    document.body.classList.remove('dark');
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    console.log('Toggling Dark Mode:', this.isDarkMode);
+    document.body.classList.toggle('dark', this.isDarkMode);
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+  }
+
 }
